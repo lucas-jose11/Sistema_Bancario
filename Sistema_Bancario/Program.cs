596eff8d90834d0cd928771ea1f199c0ac0b96f4
Lucas José <lucasjosesilva012@gmail.com>
2025-03-14 11:30:22 -0300

    Continuar

--- Sistema_Bancario/Program.cs
+++ Sistema_Bancario/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Sistema_Bancario
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Atividade 2");
         }
     }
 }
