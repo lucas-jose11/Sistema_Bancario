3cf5f95788ed7b1f0de7b60e390cd631b3cd2ba4
Lucas José <lucasjosesilva012@gmail.com>
2025-03-14 08:22:20 -0300

    Segunda atividade - Sistema Bancário

---
+++ Sistema_Bancario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sistema_Bancario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
